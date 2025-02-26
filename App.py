133f73fb05cd70e0de113ce5f8b96a80f07cb8d8
GdXcIi <gdupuis92100@gmail.com>
2025-02-26 12:00:55 +0100

    Update App.py

--- App.py
+++ App.py
@@ -21,7 +21,7 @@ st.markdown("""
 # --- Logo ---
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("Logo.png", width=100, use_container_width=True)
+    st.image("Logo.png", width=75, use_container_width=True)
 
 st.sidebar.image("Logo.png", width=50)
 st.markdown("""
